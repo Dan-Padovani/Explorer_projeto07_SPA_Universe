4d9ab0638a037c65b10b408f8534d302ff622844
Dan-Padovani <danpadovani84@gmail.com>
2022-08-13 23:08:52 -0300

    menu mobile finished

--- app/js/index.js
+++ app/js/index.js
@@ -9,3 +9,18 @@ router.add(404, '/app/pages/404.html')
 router.handle()
 window.onpopstate = () => router.handle()
 window.route = () => router.route()
+
+
+
+/*button mobile*/
+
+const buttomMobile = document.querySelector('.btn-mobile')
+const menuMobile = document.querySelector('#menu')
+const menuBarsMobile = document.querySelector('#three-bars')
+
+function toggleMenu() {
+	menuMobile.classList.toggle('active')
+	menuBarsMobile.classList.toggle('active')
+}
+
+buttomMobile.addEventListener('click', toggleMenu)
